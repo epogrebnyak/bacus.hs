400d488a0565f5be5e9af2cf0640fe9edf806192
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2025-01-19 01:32:00 +0300

    tests: try Tasty tests (may take longer)

--- test/Testing.hs
+++ test/Testing.hs
@@ -1,21 +1,38 @@
 module Main (main) where
 
+import Test.Tasty
+import Test.Tasty.HUnit
 import Bacus
 import Bacus.Types
 import Bacus.Event
 import qualified Example as E
-import Test.HUnit
 import Data.Either (isRight)
 
-testStreamBalances :: Test
-testStreamBalances = TestCase $ do
-  let (errs, book) = runP (concat E.exampleStream)
-  let testBalances = toBalances $ ledgerB book
-  if isRight errs
-    then assertEqual "Balances are equal" E.exampleBalances testBalances
-    else assertFailure $ "Error: " ++ show errs
-
 main :: IO ()
-main = do
-  _ <- runTestTT testStreamBalances
-  return ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "All Tests" 
+  [ balanceTests
+  , emptyBookTests
+  ]
+
+balanceTests :: TestTree
+balanceTests = testGroup "Balance Tests"
+  [ testCase "Stream balances match example" $ do
+      let (errs, book) = runP (concat E.exampleStream)
+      let testBalances = toBalances $ ledgerB book
+      if isRight errs
+        then E.exampleBalances @=? testBalances
+        else assertFailure $ "Error: " ++ show errs
+  ]
+
+emptyBookTests :: TestTree
+emptyBookTests = testGroup "Empty Book Tests"
+  [ testCase "Empty stream produces empty balances" $ do
+      let (errs, book) = runP []
+      let testBalances = toBalances $ ledgerB book
+      if isRight errs
+        then mempty @=? testBalances
+        else assertFailure $ "Error: " ++ show errs
+  ]
